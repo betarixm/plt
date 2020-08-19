cdeee75d1dc90cc4c2a16f06c48f20688e0c734f
whysw <ysw1109@postech.ac.kr>
2020-08-20 02:00:48 +0900

    fixed error when submitting invalid Flag

--- app/flag/apps.py
+++ app/flag/apps.py
@@ -6,10 +6,11 @@ Team = get_user_model()
 
 
 def check_flag(team: Team, flag_str: str):
-    flag = Flag.objects.filter(flag=flag_str).exclude(teams__username=team.username)[0]
+    flag = Flag.objects.filter(flag=flag_str).exclude(teams__username=team.username)
     if not flag:
         return False, None
-
+    flag = flag[0]
+    
     team.add_score(flag.score)
     flag.teams.add(team)
 
