78c78fd10cbfb1e448eaeb5739d56a45ca318f43
beta-lux <beta.underalpha@gmail.com>
2021-01-16 20:19:48 +0900

    Alert Component

---
+++ front/src/component/Alert.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface AlertProps {
+    type: "success"|"warning";
+    message: string;
+}
+
+interface AlertStates {
+
+}
+
+class Alert extends React.Component<AlertProps, AlertStates> {
+    render() {
+        return (
+            <div className={"alert " + this.props.type}>
+                <span className={"material-icons-round"}>
+                    {this.props.type === "warning" ? "warning" : ""}
+                </span>
+                <div className={"message"}>
+                    {this.props.message}
+                </div>
+            </div>
+        )
+    }
+}
